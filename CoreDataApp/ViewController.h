9145b4e04018f14c7a19f5b208102b2d3ffed516
Helina Devaraj <helinadevaraj@helinas-imac.local>
2018-07-30 15:25:03 +0530

    Initial Commit

---
+++ CoreDataApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataApp
+//
+//  Created by Helina Devaraj on 7/30/18.
+//  Copyright © 2018 Helina Devaraj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreDataApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CoreDataApp
+//
+//  Created by Helina Devaraj on 7/30/18.
+//  Copyright © 2018 Helina Devaraj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
